fa8c623f3792129d8287d44835379f69c14e11d2
JradIr <delarosa.321391@gmail.com>
2026-08-23 13:10:02 +0800

    first commit

---
+++ challenge 1.py
@@ -0,0 +1,26 @@
+def is_balanced(s):
+    stack = [] 
+    matching_brackets = {')': '(', ']': '[', '}': '{'}
+
+    for char in s: 
+        # 1. Is it an open bracket?
+        if char in ['(', '{', '[']:
+            stack.append(char)
+            
+        # 2. Is it a close bracket?
+        elif char in matching_brackets:
+            # Make sure the stack isn't empty before we try to pop!
+            if len(stack) == 0:
+                return False
+                
+            top_item = stack.pop()
+            
+            # Use square brackets [] to look up the match in the dictionary
+            if top_item != matching_brackets[char]:
+                return False
+                
+    # 3. Did we finish with an empty stack?
+    if len(stack) == 0:
+        return True
+    else:
+        return False
---
+++ main.py
@@ -0,0 +1,1 @@
+print(" ito nanaman ")
