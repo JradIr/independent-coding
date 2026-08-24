1222493234619ce55dd88de1f4afda586da7d729
JradIr <delarosa.321391@gmail.com>
2026-08-25 00:13:11 +0800

    vip bouncer

---
+++ vip bouncer.py
@@ -0,0 +1,9 @@
+# challenge 4
+
+line_of_people = [16, 21, 18, 14, 30]
+
+for people in line_of_people:
+    if people >= 18:
+        print("welcome")
+    elif people < 18:
+        print("sorry too young")
