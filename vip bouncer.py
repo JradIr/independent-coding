480026627cd1d9b1c5457a52700f8be146166895
JradIr <delarosa.321391@gmail.com>
2026-08-25 09:44:53 +0800

    challenge4

--- vip bouncer.py
+++ vip bouncer.py
@@ -1,9 +1,20 @@
 # challenge 4
 
-line_of_people = [16, 21, 18, 14, 30]
 
-for people in line_of_people:
-    if people >= 18:
-        print("welcome")
-    elif people < 18:
-        print("sorry too young")
+beachClub = [16, 21, 18, 14, 30, 17]
+downtownClub = [21, 20 ,19, 15]
+def bouncer_bot(age_list):
+    total_money = 0
+    people_admitted = 0
+    rejected_ages = []
+    for age in age_list:
+        if age >= 18:
+            total_money += 10
+            people_admitted += 1
+        else:
+            rejected_ages.append(age)
+    print(total_money)
+    print(people_admitted)
+    print(rejected_ages)
+bouncer_bot(beachClub)
+bouncer_bot(downtownClub)
