f9a9a581d9355b307fb73a709e00f2cc7cb88476
JradIr <delarosa.321391@gmail.com>
2026-09-01 23:14:45 +0800

    calling function

--- functions.py
+++ functions.py
@@ -15,7 +15,7 @@ print(name)"""
 
 #more parameters
 
-def greet(name, greeting="hello"):
+"""def greet(name, greeting="hello"):
     return f"{greeting}, {name}"
 
 print(greet("raven", "Hi"))
@@ -25,4 +25,26 @@ def introduce(greet, name, age, title="Mr/Mrs"):
     return f"{greet} {title} {name}, you are {age} years old."
 
 result = introduce("Hello", "raven", "20")
-print(result)
+print(result)"""
+
+#calling function inside a function
+
+def is_adult(age):
+    return age >= 18
+
+def get_entry_fee(age):
+    age = is_adult(age)
+    if age >= 18:
+        return 10
+    else:
+        return 0
+
+def greet_guest(name, greeting="Welcome"):
+    return f"{greeting}, {name}"
+
+def check_guest(name, age):
+    name = greet_guest(name)
+    age = is_adult(age)
+    return f"{name}, {age}"
+
+print(check_guest("raven", 20))
