a808cdf94433c3671d29d163be851c96a8627e6b
JradIr <delarosa.321391@gmail.com>
2026-09-01 22:50:56 +0800

    learned return in functions

--- functions.py
+++ functions.py
@@ -1,4 +1,14 @@
-def get_name(username):
-    print("Your name is", username)
+def get_name(name):
+    return name
 
-get_name("Raven")
+def is_long_word(check):
+    if len(check) > 5:
+        return True
+    else:
+        return False
+
+name = get_name("raven")
+
+check = is_long_word("qwerty")
+print(check)
+print(name)
