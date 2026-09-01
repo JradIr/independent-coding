580068f95b1052523d9772d10aa3348244556400
jr_adlr <delarosa.321391@gmail.com>
2026-09-02 06:44:39 +0800

    main challenge 2

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 # Copy this into your editor!
 
-def calculate_coffee_cost(number_of_coffees):
+def calculate_discounted_cost(number_of_coffees):
     total = number_of_coffees * 3
+
+    if number_of_coffees >= 5:
+        total -= 2
+        return total
     return total
+    # 2. Check if number_of_coffees is 5 or greater
+    # 3. If it is, subtract 2 from the total
+    # 4. Return the final total
     
     pass # <-- Delete this word and write your code!
-print(calculate_coffee_cost(3))
+
+# --- Test cases ---
+# When you run your code, these should print the correct numbers!
+print(calculate_discounted_cost(3)) # Should print 9
+print(calculate_discounted_cost(5)) # Should print 13 
+print(calculate_discounted_cost(10)) # Should print 28
